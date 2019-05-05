1da321ec095cfe0703d82ff235b54388dc0baec1
Zalkin <Zalkin.viktor@mail.ru>
2019-05-05 12:43:05 +0300

    S/I. Add opening client dir on request

--- server/index.js
+++ server/index.js
@@ -1,4 +1,9 @@
 const express = require("express");
+const path = require("path");
 const app = express();
+
+const clientPath = path.join(__dirname, "../client");
+
+app.use(express.static(clientPath));
 
 app.listen(808, () => { console.log("Server has been started!") })
