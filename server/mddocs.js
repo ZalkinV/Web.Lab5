7f9b18fa3665b75a275dce3f286300689f0fc115
Zalkin <Zalkin.viktor@mail.ru>
2019-05-05 16:06:08 +0300

    S/MD. Add getDocsCollection() & MongoDB connection

--- server/mddocs.js
+++ server/mddocs.js
@@ -1,10 +1,22 @@
 const express = require("express");
+const mongodb = require("mongodb");
+
+
+const connectionString = "mongodb://localhost:27017";
 
 const router = express.Router();
+
 
 router.get("/", (req, res) => 
 {
     res.send("Hello, World!");
 });
+
+
+function getDocsCollection()
+{
+    const connection = mongodb.MongoClient.connect(connectionString);
+    return connection.db("mddocs").collection("docs");
+}
 
 module.exports = router;
