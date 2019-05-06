923209d33bdf5b40004d03ef2fe11ee4e0d2af78
Zalkin <Zalkin.viktor@mail.ru>
2019-05-06 08:36:17 +0300

    S/I. Add using cors

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const path = require("path");
+const cors = require("cors");
+
+
 const app = express();
 
-
 app.use(express.json());
+app.use(cors());
 
 const mddocs = require("./mddocs");
 app.use("/api/mddocs", mddocs);
