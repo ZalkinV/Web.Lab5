86c14fb42ecf678f50c9b045104f653e943eafb6
Zalkin <Zalkin.viktor@mail.ru>
2019-05-06 06:56:18 +0300

    C/S/DS. Implement getDocuments()

--- client/src/DocumentService.js
+++ client/src/DocumentService.js
@@ -5,6 +5,22 @@ const url = "http://localhost:808/api/mddocs";
 
 class DocumentService
 {
+    static getDocuments()
+    {
+        return new Promise(async (resolve, reject) =>
+        {
+            try
+            {
+                const result = await axios.get(url);
+                const data = result.data;
+                resolve(data);
+            }
+            catch (e)
+            {
+                reject(e);
+            }
+        });
+    }
 }
 
 export default DocumentService;
