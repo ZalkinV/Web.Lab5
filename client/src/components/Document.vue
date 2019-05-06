caf8bbf6fbabd01459492be831fe6ed630084eaf
Zalkin <Zalkin.viktor@mail.ru>
2019-05-06 07:56:11 +0300

    C/S/CMP/D.Template. Add v-for for documents

--- client/src/components/Document.vue
+++ client/src/components/Document.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="documents-container">
-    <div class="document">
+    <div class="document"
+    v-for="document in documents"
+    v-bind:item="document"
+    v-bind:key="document._id">
     </div> 
   </div>
 </template>
